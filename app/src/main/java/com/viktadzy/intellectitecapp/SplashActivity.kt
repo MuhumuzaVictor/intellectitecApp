e629801bd3e755ee75d88469beab4e3dfca9dec1
VIKTA DZY <viktamuhumuza@gmail.com>
2022-06-21 00:10:07 +0300

    Initial commit

---
+++ app/src/main/java/com/viktadzy/intellectitecapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.viktadzy.intellectitecapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val splash_time_out=4000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(
+            {
+                val i= Intent(this,NavigationActivity::class.java)
+                startActivity(i)
+            },splash_time_out
+        )
+    }
+}
