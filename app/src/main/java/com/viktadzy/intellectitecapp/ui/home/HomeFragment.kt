8daf0ec4f37722d26442c7fe3befb41abc83a507
VIKTA DZY <viktamuhumuza@gmail.com>
2022-06-26 17:40:42 +0300

    initial commit

--- app/src/main/java/com/viktadzy/intellectitecapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/viktadzy/intellectitecapp/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,6 +29,7 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
@@ -40,3 +42,4 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
+
